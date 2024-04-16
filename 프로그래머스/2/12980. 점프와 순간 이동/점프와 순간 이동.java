074fbc4b56bcfab35bcf38cea5f963c8b7180354
tjwlgns15 <136657819+tjwlgns15@users.noreply.github.com>
2024-04-16 16:17:11 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 1;
+        while(n > 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n -= 1;
+                ans++;
+            }    
+        }
+
+        return ans;
+    }
+}
