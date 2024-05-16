02cd5b1318606f6934603a9ffba011536beace3c
tjwlgns15 <136657819+tjwlgns15@users.noreply.github.com>
2024-05-16 14:02:12 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.90 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder ternary = new StringBuilder();
+        String ternary = "";
         while (n > 0) {
-            ternary.append(n % 3);
+            ternary += n % 3;
             n /= 3;
         }
 
-        int answer = 0;
-        String strTernary = ternary.toString();
-        for (int i = 0; i < strTernary.length(); i++) {
-            char digitChar = strTernary.charAt(i);
+        int answer = 0;        
+        for (int i = 0; i < ternary.length(); i++) {
+            char digitChar = ternary.charAt(i);
             int digit = Character.getNumericValue(digitChar);
-            answer += (digit * Math.pow(3, strTernary.length() - 1 - i));
+            answer += (digit * Math.pow(3, ternary.length() - 1 - i));
         }
 
         return answer;
